f9e2e552ffb311e04446009706c58b8e8ad4b667
kirill <116079986+kirill-XII@users.noreply.github.com>
2023-02-07 15:46:05 +0300

    different versions

--- src/App.js
+++ src/App.js
@@ -1,25 +1,75 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
+//Передается тольк один раз(снижение нагрузки)
+// function App() {
+// 	const [count, setCount] = useState(() => {
+// 		console.log('run function 1 time')
+// 		return 4
+// 	})
+// 	let decrementCount = () => {
+// 		setCount(prevCount => prevCount - 1)
+// 	}
+// 	let incrementCount = () => {
+// 		setCount(prevCount => prevCount + 1)
+// 	}
+// 	return (
+// 		<div>
+// 			<button onClick={decrementCount}>-</button>
+// 			<span>{count}</span>
+// 			<button onClick={incrementCount}>+</button>
+// 		</div>
+// 	)
+// }
+
+// export default App
+
+//Передается при каждом нажатии
+// function countInitial() {
+// 	console.log('run function 1 time')
+// 	return 4
+// }
+
+// function App() {
+// 	const [count, setCount] = useState(countInitial())
+// 	let decrementCount = () => {
+// 		setCount(prevCount => prevCount - 1)
+// 	}
+// 	let incrementCount = () => {
+// 		setCount(prevCount => prevCount + 1)
+// 	}
+// 	return (
+// 		<div>
+// 			<button onClick={decrementCount}>-</button>
+// 			<span>{count}</span>
+// 			<button onClick={incrementCount}>+</button>
+// 		</div>
+// 	)
+// }
+
+// export default App
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [state, setState] = useState({ count: 4, theme: 'blue' })
+	const count = state.count
+	const theme = state.theme
+
+	function decrementCount() {
+		setState(prevState => {
+			return { ...prevState, count: prevState.count - 1 }
+		})
+	}
+	function incrementCount() {
+		setState(prevState => {
+			return { ...prevState, count: prevState.count + 1 }
+		})
+	}
+	return (
+		<div>
+			<button onClick={decrementCount}>-</button>
+			<span>{count}</span>
+			<span>{theme}</span>
+			<button onClick={incrementCount}>+</button>
+		</div>
+	)
 }
 
-export default App;
+export default App
